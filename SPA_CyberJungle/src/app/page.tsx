87c3c33bff23d8d52b27382eb0de73e813390d49
ricardofpe <ricardofonseca_1@hotmail.com>
2024-10-03 18:51:12 -0300

    Add map of products

--- SPA_CyberJungle/src/app/page.tsx
+++ SPA_CyberJungle/src/app/page.tsx
@@ -3,13 +3,10 @@ import { products } from "@/core";
 
 export default function Home() {
   return (
-   <div className="flex gap-5 flex-wrap">
-   <ProductItem product={products[0]}/>
-   <ProductItem product={products[1]}/>
-   <ProductItem product={products[2]}/>
-   <ProductItem product={products[3]}/>
-   <ProductItem product={products[4]}/>
-   <ProductItem product={products[5]}/>
-   </div>
+    <div className="flex gap-5 flex-wrap">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
   );
 }
